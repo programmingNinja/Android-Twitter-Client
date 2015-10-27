f1f738f5a977ea16de05a16a8a46e0afe8244cc6
Rshah4 <rshah2@walmartlabs.com>
2015-10-26 23:37:29 -0700

    full client

---
+++ app/src/main/java/com/codepath/apps/TwttrClient/TweetComposeDialogListener.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.TwttrClient;
+
+/**
+ * Created by rshah4 on 10/23/15.
+ */
+public interface TweetComposeDialogListener {
+    void onFinishedComposingTweet(String inputText);
+}
